1b0bbf8f89422576d4ccc81c77246ade098f7751
Ashutosh_Gajankush <agajankush.github@gmail.com>
2025-03-19 19:41:54 -0400

    Seperated the enhance resume function so it doesnt do it when we upload the document

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile, Depends
+from fastapi import FastAPI, File, UploadFile, Depends, BackgroundTasks
 import shutil
 import models
 from ai_enhancer import enhance_resume, enhance_resume_groq
@@ -6,11 +6,16 @@ from resume_parser import extract_text_from_pdf
 from database import engine, SessionLocal
 from models import Resume
 from sqlalchemy.orm import Session
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
 app = FastAPI()
 models.Base.metadata.drop_all(bind=engine)
 models.Base.metadata.create_all(bind=engine)
+upload_dir = os.environ.get("UPLOAD_DIR")
+os.makedirs(upload_dir, exist_ok=True)
 
 def get_db():
     db = SessionLocal()
@@ -19,18 +24,33 @@ def get_db():
     finally:
         db.close()
 
+
+
 @app.post("/upload_resume/")
 async def upload_resume(file: UploadFile = File(...), db: Session = Depends(get_db)):
-    file_path = file.filename
+    file_path = f"{upload_dir}/{file.filename}"
     with open(file.filename, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
     
     resume_text = extract_text_from_pdf(file_path)
-    enhanced_resume = enhance_resume_groq(resume_text)
+    
 
-    new_resume = Resume(filename=file.filename, original_text=resume_text, enhanced_text=enhanced_resume)
+    new_resume = Resume(filename=file.filename, original_text=resume_text)
     db.add(new_resume)
     db.commit()
     db.refresh(new_resume)
 
-    return {"id": new_resume.id ,"original": resume_text, "enhanced": enhanced_resume}
+    return {"resume_id": new_resume.id ,"original_text": resume_text}
+
+@app.post("/enhance/{resume_id}")
+async def enhance_resume_api(resume_id: int, background_tasks: BackgroundTasks, db: Session=Depends(get_db)):
+    resume = db.query(Resume).filter(Resume.id==resume_id).first()
+    if not resume:
+        return {"error": "Resume not found"}
+
+    background_tasks.add_task(process_enhancement, "resume_id": resume_id)
+
+def process_enhancement(resume, db):
+    enhanced_text = enhance_resume_groq(resume.original_text)
+    resume.enhanced_text = enhanced_text
+    db.commit()
